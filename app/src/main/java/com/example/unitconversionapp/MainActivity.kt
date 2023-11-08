38464398ae396e3509e345c64d3b0692eb8462f7
Growth-Programmer <147464744+Growth-Programmer@users.noreply.github.com>
2023-11-08 13:05:26 -0500

    Commit 0002

--- app/src/main/java/com/example/unitconversionapp/MainActivity.kt
+++ app/src/main/java/com/example/unitconversionapp/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.unitconversionapp
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconversionapp.ui.theme.UnitConversionAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,11 +35,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             UnitConversionAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    UnitConverter()
                 }
             }
         }
@@ -30,17 +44,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun UnitConverter() {
+    Column (modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Text("Unit Converter")
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "", onValueChange = {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UnitConversionAppTheme {
-        Greeting("Android")
+        })
+        Row {
+            Box {
+                Button(onClick = { }) {
+                    Text("Select")
+                    Icon(
+                        Icons.Default.ArrowDropDown,
+                        contentDescription = ""
+                    )
+                }
+            }
+            Box {
+                Button(onClick = { }) {
+                    Text("Select")
+                    Icon(
+                        Icons.Default.ArrowDropDown,
+                        contentDescription = ""
+                    )
+                }
+            }
+        }
+    }
+
+
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun UnitConverterPreview() {
+        UnitConverter()
+
     }
 }
